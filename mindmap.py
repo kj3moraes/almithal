5a9293c721c3976911841b48c0abd6e912311c00
Keane Moraes <keanejonathan3@gmail.com>
2023-04-22 05:12:10 +0000

    added the function to generate relations

--- mindmap.py
+++ mindmap.py
@@ -9,6 +9,33 @@ class MindMap:
     def __init__(self):
         openai.api_key = os.getenv("OPENAI_API_KEY")
         
+    def get_connections(self, text_chunks_libs:dict) -> list:
+        
+        state_prompt = open("./prompts/mindmap.prompt")
+        PROMPT = state_prompt.read()
+        state_prompt.close()
+        
+        
+        for key in text_chunks_libs:
+            for text_chunk in text_chunks_libs[key]:
+                PROMPT = PROMPT.replace("$prompt", text_chunk)
+                
+                response = openai.Completion.create(
+                    engine="text-davinci-003",
+                    prompt = PROMPT,
+                    temperature=0.5,
+                    max_tokens=2048,
+                    top_p=1,
+                    frequency_penalty=0.0,
+                    presence_penalty=0.0,
+                )
+                
+                print("response is ", response.choices[0].text)
+                
+                
+        return []
+            
+        
     def generate_graph(self, text_chunks_libs:dict):
         graph = graphviz.Digraph()
         graph.edge('run', 'intr')
@@ -24,5 +51,5 @@ class MindMap:
         graph.edge('runswap', 'runmem')
         graph.edge('new', 'runmem')
         graph.edge('sleep', 'runmem')
-
+        self.get_connections(text_chunks_libs)
         st.graphviz_chart(graph)
